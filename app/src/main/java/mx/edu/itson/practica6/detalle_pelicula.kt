bd1818980f9727dd2cd9f485e71c9d44176238bf
Victoro2304 <victor.encinas244821@potros.itson.edu.mx>
2025-03-06 21:35:12 -0700

    Cambios seleccionar asientos

--- app/src/main/java/mx/edu/itson/practica6/detalle_pelicula.kt
+++ app/src/main/java/mx/edu/itson/practica6/detalle_pelicula.kt
@@ -1,6 +1,8 @@
 package mx.edu.itson.practica6
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -14,15 +16,24 @@ class detalle_pelicula : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_detalle_pelicula)
 
+        val btnBuyTicket: Button = findViewById(R.id.buyTickets)
+
         val iv_pelicula_image: ImageView = findViewById(R.id.iv_pelicula_imagen)
         val tv_nombre_pelicula: TextView = findViewById(R.id.tv_nombre_pelicula)
         val tv_pelicula_desc : TextView = findViewById(R.id.tv_pelicula_desc)
+        val tv_seat_left : TextView = findViewById(R.id.seatLeft)
 
         val bundle = intent.extras
         if (bundle!=null){
             iv_pelicula_image.setImageResource(bundle.getInt("header"))
             tv_nombre_pelicula.setText(bundle.getString("titulo"))
             tv_pelicula_desc.setText(bundle.getString("sinopsis"))
+            tv_seat_left.setText(bundle.getString("seats"))
+        }
+
+        btnBuyTicket.setOnClickListener {
+            var intent : Intent = Intent(this, SeatSelection::class.java)
+            startActivity(intent)
         }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
